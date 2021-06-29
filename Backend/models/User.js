e1b9f4c12a83d87cc6304e6141b59d83bfb65d12
Arnaud <asallafranque@protonmail.com>
2021-06-29 17:37:13 +0200

    Modèle utilisateur

---
+++ Backend/models/User.js
@@ -0,0 +1,24 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize =  new Sequelize('mysql::memory:');
+
+// module.exports = (sequelize, DataTypes) => {
+const User = sequelize.define('User', {
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+// return User;
+// };
